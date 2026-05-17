c306166f6aa8c1547ea0e8e3aef5d78c7833167f
Hafthor <git@hafthor.net>
2026-05-16 21:27:43 -0400

    readme

--- JBenchmark.cs
+++ JBenchmark.cs
@@ -27,10 +27,14 @@ public class JBenchmark {
 
     [Benchmark] // 147.4ns 824B
     public string Serialize() => J.ToString();
+    
+    // combined 1132.3ns 5760B
 
     //[Benchmark] // 1501.7ns 6664B - 1.525x time 1.350x mem
     //public object NParse() => Newtonsoft.Json.JsonConvert.DeserializeObject(D);
 
     //[Benchmark] // 361.6ns 1728B - 2.453x time 2.097x mem
     //public string NSerialize() => Newtonsoft.Json.JsonConvert.SerializeObject(J2);
+    
+    // combined 1863.3ns 8392B - 1.647x time 1.456x mem / 39.28% less time 31.32% less mem
 }
